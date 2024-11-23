cbfe6e382b0d5aa5196be5f696ebe08769a226d9
fredericbob <lalafrederic05@gmail.com>
2024-11-23 20:05:27 +0300

    sprint1

---
+++ Web/Js/data.js
@@ -0,0 +1,6 @@
+export const pays = [
+    "Afghanistan", "Afrique du Sud", "Aland (Îles)", "Albanie", "Algérie",
+    "Allemagne", "Andorre", "Angola", "Anguilla", "Antarctique",
+    "Antigua-et-Barbuda", "Arabie saoudite", "Argentine", "Arménie", "Aruba",
+    "Australie", "Autriche", "Azerbaïdjan", "Bahamas", "Bahreïn"
+];
